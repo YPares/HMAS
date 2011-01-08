dc98057effcc55e265587276fc54fb51262a1766
Bastien <bast.bonnet@laposte.net>
2011-01-08 16:02:37 +0100

    Ajout dossier dev

---
+++ dev/TestImage.java
@@ -0,0 +1,37 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class TestImage {
+
+    public static void main(String[] args) throws IOException {
+       displayBoolArray(urlToBoolArray("1.png")); 
+    }
+
+    public static boolean[][] urlToBoolArray(String url) throws IOException {
+        File f = new File(url);
+        BufferedImage i = null;
+        i = ImageIO.read(f);
+        int c;
+        int blue;
+        boolean boolArray[][] = new boolean[i.getWidth()][i.getHeight()];
+
+        for (int x = 0; x < i.getWidth(); x++) {
+            for (int y = 0; y < i.getHeight(); y++) {
+                blue = i.getRGB(x,y) & 0x000000ff ;
+                boolArray[x][y] = (blue != 255);
+            }
+        }
+        return boolArray;
+    }
+
+    public static void displayBoolArray(boolean[][] b) {
+        for (int y=0; y < b[0].length; y++) {
+            for (int x=0; x < b.length; x++) {
+                 System.out.print(b[x][y] ? "X" : " ");   
+            }
+            System.out.println("");
+        }
+    }
+}
