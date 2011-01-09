66e1dd5b25251a9353b3bcbcaaf570e3d84ba064
Bastien <bast.bonnet@laposte.net>
2011-01-09 22:44:18 +0100

    PixelAgent

--- src/hmas/agents/PixelAgent.java
+++ src/hmas/agents/PixelAgent.java
@@ -12,8 +12,6 @@ public class PixelAgent extends Agent
     }
     public void step()
     {
-        int stepX, stepY;
-
         if (getPixel(getPosition()) && ! collidingWithMe().iterator().hasNext())
         {
             setFixed();
