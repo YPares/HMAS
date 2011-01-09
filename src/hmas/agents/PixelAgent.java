d9701ff85a444f8196c5e803a7ccdfef82bfc7bf
Bastien <bast.bonnet@laposte.net>
2011-01-09 19:15:03 +0100

    Ajout PixelAgent

---
+++ src/hmas/agents/PixelAgent.java
@@ -0,0 +1,16 @@
+package hmas.agents;
+
+import hmas.*;
+
+public class PixelAgent extends Agent
+{
+
+   public PixelAgent (World world, int level, Vector position, Vector diagonal)
+   {
+       super(world, level, position, diagonal);
+   }
+   public void step()
+   {
+
+   }
+}
